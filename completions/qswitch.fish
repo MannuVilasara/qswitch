55d0251c48420eab951abafddc5ac07c44aef41b
MannuVilasara <mannuvilasara@gmail.com>
2025-11-30 23:23:32 +0530

    feat: configurable

--- completions/qswitch.fish
+++ completions/qswitch.fish
@@ -1,4 +1,9 @@
-set -l flavours ii caelestia noctalia
+set -l flavours
+if command -q qswitch
+    set flavours (qswitch --help 2>/dev/null | awk '/Available flavours:/{flag=1; next} flag && /^  /{print $1} /^$/{exit}')
+else
+    set flavours ii caelestia noctalia
+end
 
 complete -c qswitch --erase
 
@@ -6,3 +11,4 @@ complete -c qswitch -f -a "$flavours" -d "Available flavours"
 
 complete -c qswitch -l help -d "Show help"
 complete -c qswitch -s h -d "Show help"
+complete -c qswitch -l list -d "List available flavours"
