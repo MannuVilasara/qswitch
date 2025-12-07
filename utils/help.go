9c011cd575cdaa467dcfb06635671fbe18002881
MannuVilasara <mannuvilasara@gmail.com>
2025-12-07 11:15:06 +0530

    remove manual help command

--- utils/help.go
+++ utils/help.go
@@ -17,23 +17,3 @@ After setup, you can run qswitch normally.
 
 To bypass this message (not recommended), use the --itrustmyself flag.`)
 }
-
-func Help(config Config) {
-	fmt.Println(`Usage:
-  qswitch <flavour>       Switch to a specific flavour
-  qswitch                 Cycle to the next flavour
-
-Available flavours:`)
-	for _, f := range config.Flavours {
-		fmt.Println("  " + f)
-	}
-	fmt.Println(`
-Options:
-  --help, -h             Show this help message
-  --list                 List available flavours
-  --current              Show current flavour
-  --panel                Toggle panel
-  apply --current        Apply current flavour configuration
-  --itrustmyself         Bypass setup check (use with caution)
-  --switch-keybinds      Switch only keybinds for a flavour`)
-}
